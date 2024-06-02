08f01144f797ef9074b38f00b1153c65b6d87c32
quantumeduardo <jorgehernandez@berkeley.edu>
2024-06-02 10:16:37 -0700

    new project next js

---
+++ app/dashboard/page.js
@@ -0,0 +1,7 @@
+import { Button } from "../ui/button"
+import React from "react"
+import Link from "next/link"
+
+export default function Page(){
+    return <Link href='/'><Button>GO BACK TO LOGIN</Button> </Link>
+}
